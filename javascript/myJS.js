ea7c8045c51901eb6ec396aeac99c114b19fe49f
Majid Babaeifar <88742101+MajidBabaeifar@users.noreply.github.com>
2021-09-20 01:19:25 +0430

    new

--- javascript/myJS.js
+++ javascript/myJS.js
@@ -1,15 +1,46 @@
 let HeroBtn = document.getElementById('hero-btn');
+let x = 0;
 HeroBtn.setAttribute('href', '');
 
-function showbtn() {
-    HeroBtn.style.right = "0";
-    HeroBtn.setAttribute('href', 'contactUs.html');
+function toggleHeroBtn() {
+    if (x == 0) {
+        HeroBtn.style.right = "0";
+        HeroBtn.setAttribute('href', '');
+        x = 1;
+    } else {
+        HeroBtn.style.right = "-65px";
+        HeroBtn.setAttribute('href', '');
+        x = 0;
+    }
 }
 
-function hidebtn() {
-    HeroBtn.style.right = "-65px";
-    HeroBtn.setAttribute('href', '');
-}
+HeroBtn.addEventListener("click", toggleHeroBtn);
+
+
+// /////////////////////////////////////
+let checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('click', function() {
+    if (checkbox.value == 'on') {
+        console.log = 'majidjjjj';
+    }
+})
+
+
+// let HeroBtn = document.getElementById('hero-btn');
+// HeroBtn.setAttribute('href', '');
+
+// function showbtn() {
+//     HeroBtn.style.right = "0";
+//     HeroBtn.setAttribute('href', 'contactUs.html');
+// }
+
+// function hidebtn() {
+//     HeroBtn.style.right = "-65px";
+//     HeroBtn.setAttribute('href', '');
+// }
+// HeroBtn.addEventListener("mouseover", showbtn);
+// HeroBtn.addEventListener("mouseleave", hidebtn);
 
 
 
